3693de737c3dd77fbf0f7924763fb55ebdf7d336
Nichita Stefanita <nichita.stefanita@gmail.com>
2020-07-12 23:52:29 +0300

    Initial Commit

---
+++ Explore/ExploreApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploreApp.swift
+//  Explore
+//
+//  Created by Nichita Stefanita on 7/12/20.
+//
+
+import SwiftUI
+
+@main
+struct ExploreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
